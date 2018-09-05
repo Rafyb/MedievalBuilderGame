e33f12d4b9839eee64d00863597ef1b547e008d7
duhamela <duhamela@acajou08.iut-infobio.priv.univ-lille1.fr>
2018-09-05 14:58:51 +0200

    armoniser partie et besoin

--- src/simulation/Besoin.java
+++ src/simulation/Besoin.java
@@ -7,11 +7,8 @@ public class Besoin {
 	
 	public static final int besoinEnNourriture = 3;
 	
-	public static int stockNourriture = 0;
 	public static int totalBesoinEnNourriture = 0;
-	public static int argent = 0;
-	public static int population = 0;
-	public static int populationMax = 0;
+
 	
 	public static void actualiser (Parcelle monde[][]) {
 		//lire les production
@@ -19,26 +16,26 @@ public class Besoin {
 			for (int j = 0 ; j<monde[i].length ; j++) {
 				if (monde[i][j]!=null) {
 					if (monde[i][j].getClass().getName()=="Mine") {
-						argent += monde[i][j].getProduction();						
+						Partie.money += monde[i][j].getProduction();						
 					}
 					if (monde[i][j].getClass().getName()=="Champs") {
-						stockNourriture += monde[i][j].getProduction();						
+						Partie.food += monde[i][j].getProduction();						
 					}
 					
 					if (monde[i][j].getClass().getName()=="Maison") {
-						populationMax += monde[i][j].getPlaceMax();						
+						Partie.population += monde[i][j].getPlaceMax();						
 					}					
 				}
 			}
 		}
 		//deduire les besoins
-		totalBesoinEnNourriture = population*besoinEnNourriture;
-		stockNourriture -= totalBesoinEnNourriture;
+		totalBesoinEnNourriture = Partie.population*besoinEnNourriture;
+		Partie.food -= totalBesoinEnNourriture;
 		
 	}
 	
 	private static double satisfactionNourriture () {
-		return Math.min( stockNourriture / totalBesoinEnNourriture , 100);
+		return Math.min( Partie.food / totalBesoinEnNourriture , 100);
 	}
 	
 	public static double satisfactionBonheur() {
