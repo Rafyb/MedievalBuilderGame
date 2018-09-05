947ce6dfcd5a55c04a834a65313659a9a4bfb6df
BauvinRaphael <raphael.bauvin@etudiant.univ-lille1.fr>
2018-09-05 16:49:18 +0200

    taverne fini

--- src/entite/Taverne.java
+++ src/entite/Taverne.java
@@ -2,36 +2,17 @@ package entite;
 
 public class Taverne extends Parcelle {
 	private static int coutPlacer = 500;
-	private final int coutSuppr = 100;
-	private final int placeMax = 0;
-	private final int production = 15;
+
 	
 	public Taverne() {
+		cout = coutPlacer;
 		coutPlacer += 200;
+		coutSuppr = 100;
 		construit = true;
+		placeMax = 0;
+		icon = "T";
+		production = 15;
 	}
 
-	public int getCoutPlacer() {
-		return coutPlacer;
-	}
 
-	public int getCoutSuppr() {
-		return coutSuppr;
-	}
-
-	public int getPlaceMax() {
-		return placeMax;
-	}
-
-	public int getProduction() {
-		return production;
-	}
-	
-	public boolean getConstruit() {
-		return construit;
-	}
-	
-	public String getIcon() {
-		return "T";
-	}
 }
