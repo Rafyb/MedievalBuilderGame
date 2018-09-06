4656eb3af455610e6d0ccb2648ba4bd4b0249ffa
Poteau Alexis <alexis.poteau@etudiant.univ-lille1.fr>
2018-09-06 09:58:09 +0200

    correction de bogues

--- src/monde/Plateau.java
+++ src/monde/Plateau.java
@@ -64,7 +64,7 @@ public class Plateau {
 			plateau[x][y] = batiment;
 			Partie.money -= batiment.getCoutPlace();
 		}
-		else if (plateau[x][y].getIcon().equals("^") && batiment.getIcon().equals("M")) {
+		else if (plateau[x][y].getIcon().equals("^") && batiment.getIcon().equals("⩄")) {
 			plateau[x][y] = batiment;
 			batiment.setProduction(batiment.getProduction() + 50);
 			Partie.money -= batiment.getCoutPlace();
