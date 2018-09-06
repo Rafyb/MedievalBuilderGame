2e067426e2bf5852cd46b14b9d8c56b8f89cbada
duhamela <duhamela@acajou05.iut-infobio.priv.univ-lille1.fr>
2018-09-06 09:04:21 +0200

    rapport des production/consomations

---
+++ src/simulation/rapport.java
@@ -0,0 +1,31 @@
+package simulation;
+
+import entite.Parcelle;
+import systeme.Partie;
+
+public class rapport {
+	
+	public static int rapBesoinNourriture=0;
+	public static int rapProdNourriture=0;
+	public static int rapProdArgent=0;
+
+	public static void estimer (Parcelle monde[][]) {
+		rapBesoinNourriture=Partie.population*Besoin.besoinEnNourriture;
+		rapProdNourriture=0;
+		rapProdArgent=0;
+		
+		for (int i = 0 ; i<monde.length ; i++) {
+			for (int j = 0 ; j<monde[i].length ; j++) {
+				if (monde[i][j].getIcon()!=null) {
+					if (monde[i][j].getIcon().equals("⩄")) { // mine
+						rapProdArgent += monde[i][j].getProduction();						
+					}
+					if (monde[i][j].getIcon().equals("≋")) { //Champ
+						if ( !temp.saison().equals("hiver") ) rapProdNourriture += monde[i][j].getProduction();
+					}
+					
+				}
+			}
+		}
+	}
+}
